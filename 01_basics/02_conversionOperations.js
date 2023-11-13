3b7245bf4cae56562f0aabb610328dc212de896c
Vishal Jagadale <84771108+Vishalj633@users.noreply.github.com>
2023-11-13 05:41:15 +0000

    Conversion of Datatypes

---
+++ 01_basics/02_conversionOperations.js
@@ -0,0 +1,29 @@
+
+let score = null;
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let convertedNumber = Number(score);
+
+// console.log(typeof convertedNumber);
+// console.log(convertedNumber);
+
+
+// "33" => 33
+// "33aaa" => NaN
+// null => 0
+// undefined => NaN
+// true => 1, Flase => 0
+
+let isLoggedIn = "Vishal";
+
+let booleanLoggedIn = Boolean(isLoggedIn);
+
+console.log(booleanLoggedIn);
+
+
+// 1 => true
+// 0 => flase
+// "" => flase
+// "Vishal" => true
